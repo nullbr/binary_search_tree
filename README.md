e868e7a5e9c6158c616799172ee9119758ede101
nullbr <bmarianoleite3@gmail.com>
2022-05-19 16:41:27 -0300

    working on build tree method

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# binary_search_tree
+## Binary search tree
+
+A project with The Odin Project
+
+## Assignment
+
+You’ll build a balanced BST in this assignment. Do not use duplicate values because they make it more complicated and result in trees that are much harder to balance. Therefore, be sure to always remove duplicate values or check for an existing value before inserting.
--- lib/tree.rb
+++ lib/tree.rb
@@ -1,8 +1,16 @@
+require_relative 'node'
+
 # Build a Tree class which accepts an array when initialized.
 # The Tree class should have a root attribute which uses the return value of #build_tree which you’ll write next.
-
 class Tree
   def initialize(array)
-    @array = array
+    @root = build_tree(array)
+  end
+
+  def build_tree(array)
+    return if array.size < 2
+
+    mid_array = array.size / 2
+    root = Node.new(array[mid_array])
   end
 end
