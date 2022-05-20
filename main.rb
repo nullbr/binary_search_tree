93b31db885ef5edd10a9d17bdc06dd9381105507
nullbr <bmarianoleite3@gmail.com>
2022-05-19 22:20:35 -0300

    testing insert and find

--- main.rb
+++ main.rb
@@ -1,9 +1,21 @@
 require_relative 'lib/tree'
+require_relative 'lib/node'
 
 # testing binary seach tree class
 
-bsd = Tree.new([1, 2, 3, 4, 5, 6, 7])
-bsd.pretty_print
+bst = Tree.new([1, 2, 3, 4, 5, 6, 7])
+bst.pretty_print
 
-bsd = Tree.new([2, 5, 4, 3, 6, 7, 87, 7, 9])
-bsd.pretty_print
+bst = Tree.new([2, 5, 4, 3, 6, 7, 87, 7, 9])
+bst.pretty_print
+
+puts "\nFind a node in the tree"
+puts bst.find(4)
+puts bst.find(87)
+
+puts "\nInsert values in the tree"
+bst.insert(8)
+bst.insert(9)
+bst.insert(10)
+bst.insert(1)
+bst.pretty_print
