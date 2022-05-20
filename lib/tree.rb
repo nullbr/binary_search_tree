773089d175c0f280878ffdef69949c5fbea7e041
nullbr <bmarianoleite3@gmail.com>
2022-05-19 22:20:17 -0300

    implemented insert and find

--- lib/tree.rb
+++ lib/tree.rb
@@ -4,13 +4,47 @@ require_relative 'node'
 # The Tree class should have a root attribute which uses the return value of #build_tree which you’ll write next.
 class Tree
   def initialize(array)
-    @root = build_tree(merge_sort(array))
+    @array = merge_sort(array)
+    @root = build_tree(@array)
   end
 
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
+  end
+
+  # find a node, will help delete node
+  def find(val, current_node = @root)
+    return if current_node.nil?
+
+    return current_node if current_node.data == val
+
+    current_node.data < val ? find(val, current_node.right_child) : find(val, current_node.left_child)
+  end
+
+  # accepts a value to insert in the tree
+  def insert(value, node = @root)
+    if value < node.data
+      node.left_child.nil? ? node.left_child = Node.new(value) : insert(value, node.left_child)
+    elsif value > node.data
+      node.right_child.nil? ? node.right_child = Node.new(value) : insert(value, node.right_child)
+    end
+  end
+
+  # accepts a value to delete from the tree
+  def delete(value, node = @root)
+    return if node.nil?
+
+    if node.data == value
+      if node.right_child.nil? && node.left_child.nil?
+        node = nil
+        nil
+      elsif node.right_child.nil? || node.left_child.nil?
+        node = node.right_child.nil? ? node.left_child : node.right_child
+        nil
+      end
+    end
   end
 
   private
